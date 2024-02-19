a415080177c44f13d32931f4b9771e8f566a6ec0
Miku Kawata <kawata_miku@lightcafe.co.jp>
2024-02-19 12:56:07 +0900

    feat: Toastで成功メッセージを表示

--- src/app/exercises/02-flash-messages/contact/page.js
+++ src/app/exercises/02-flash-messages/contact/page.js
@@ -1,15 +1,30 @@
 'use client';
+import { useRouter } from 'next/navigation';
 import React from 'react';
+import { ToastContext } from '../../../../components/ToastProvider';
 
 function ContactPage() {
+  const router = useRouter();
+  const { createToast } = React.useContext(ToastContext);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    router.push('/exercises/02-flash-messages');
+    createToast(
+      `Your message was received.
+    We will get back to you shortly!`,
+      'success'
+    );
+  };
+
   return (
     <main>
-      <form>
-        <label htmlFor="name">Name:</label>
-        <input id="name" required={true} />
+      <form onSubmit={handleSubmit}>
+        <label htmlFor='name'>Name:</label>
+        <input id='name' required={true} />
 
-        <label htmlFor="message">Message:</label>
-        <textarea id="message" />
+        <label htmlFor='message'>Message:</label>
+        <textarea id='message' />
 
         <button>Submit</button>
       </form>
--- src/app/exercises/02-flash-messages/layout.js
+++ src/app/exercises/02-flash-messages/layout.js
@@ -1,11 +1,18 @@
 import React from 'react';
 
 import './styles.css';
+import ToastProvider, { ToastContext } from '../../../components/ToastProvider';
+import ToastShelf from '../../../components/ToastShelf/ToastShelf';
 
 function FlashMsgLayout({ children }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang='en'>
+      <body>
+        <ToastProvider>
+          {children}
+          <ToastShelf />
+        </ToastProvider>
+      </body>
     </html>
   );
 }
